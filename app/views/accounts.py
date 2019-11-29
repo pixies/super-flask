3ee6fa8cc3a1a22a2023f9229ba9ec2ff26f3e99
pixies <pixies@riseup.net>
2019-11-29 20:38:59 -0300

    new idea

--- app/views/accounts.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from forms.account import NewUserForm
-
-account = Blueprint('accounts', __name__)
-
-@account.route('/new')
-def new_account():
-    form = NewUserForm()
-    return render_template('account/new_user.html')
--- app/views/admin.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-admin = Blueprint('admin', __name__)
-
-@admin.route('/admin')
-def manager():
-    return render_template('admin/index.html')
